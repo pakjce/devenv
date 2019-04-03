2535e39d192fb339468c78b767ddd17e399e1845
Jun-Cheol Park <pakjce@omnious.com>
2019-04-03 14:27:49 +0900

    필수 package 설치하도록 함

--- scripts/conda-makeenv-py36-pytorch101.sh
+++ scripts/conda-makeenv-py36-pytorch101.sh
@@ -11,3 +11,6 @@ CONDA=${PYTHON_ENV_DIR}/bin/conda
 conda create -n ${CONDA_ENV_NAME} python=3.6
 
 ${CONDA} install -n ${CONDA_ENV_NAME} -y virtualenv numba cython numpy mkl mkl-include setuptools cmake scipy scikit-learn  pandas matplotlib protobuf jupyterlab jupyter pytorch=1.0.1 torchvision cudatoolkit=9.0 -c pytorch
+
+# Install Essential Packages
+${PYTHON_ENV_DIR}/bin/pip install awscli boto3 dvc
