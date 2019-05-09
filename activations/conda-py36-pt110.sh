d0cd3e2fd6f0df24ea82bbbde2e3a5efbacd9023
Jun-Cheol Park <pakjce@omnious.com>
2019-05-09 12:53:26 +0900

    pytorch1.0.1 환경 추가

---
+++ activations/conda-py36-pt110.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ENV_NAME=conda-py36-pt110
+CONDA_ENV_NAME=py36-pt110
+
+export PYTHON_ENV_DIR=$HOME/devenv/conda/envs/${CONDA_ENV_NAME}
+export PS1="(${ENV_NAME}) $PS1"
+export PATH=${PYTHON_ENV_DIR}/bin:$PATH
+export LD_LIBRARY_PATH=${PYTHON_ENV_DIR}/lib
+
--- scripts/conda-makeenv-py36-pytorch110.sh
+++ scripts/conda-makeenv-py36-pytorch110.sh
@@ -2,7 +2,7 @@
 set -ef -o pipefail
 
 source ./activations/conda.sh
-CONDA_ENV_NAME=py36-pt101
+CONDA_ENV_NAME=py36-pt110
 
 echo "PYTHON_ENV_DIR: ${PYTHON_ENV_DIR}"
 
@@ -10,7 +10,7 @@ CONDA=${PYTHON_ENV_DIR}/bin/conda
 
 conda create -n ${CONDA_ENV_NAME} python=3.6
 
-${CONDA} install -n ${CONDA_ENV_NAME} -y virtualenv numba cython numpy mkl mkl-include setuptools cmake scipy scikit-learn  pandas matplotlib protobuf jupyterlab jupyter pytorch=1.0.1 torchvision cudatoolkit=9.0 cudnn -c pytorch
+${CONDA} install -n ${CONDA_ENV_NAME} -y virtualenv numba cython numpy mkl mkl-include setuptools cmake scipy scikit-learn  pandas matplotlib protobuf jupyterlab jupyter pytorch=1.1.0 torchvision cudatoolkit=9.0 cudnn faiss-gpu -c pytorch
 
 # Install Essential Packages
 ${PYTHON_ENV_DIR}/bin/pip install awscli boto3 dvc
